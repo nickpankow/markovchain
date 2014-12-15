5d28b339a6a4655aa45737c8cc14d1734b3015c5
Nick Pankow <nickpankow@gmail.com>
2014-12-15 15:42:59 -0800

    MarkovChains now generate output using the Next() function
    Markov Chains will now generate output and change their state according
    to the probabilities given in their setup when the Next() method is
    called on the chain.

--- markovchain.go
+++ markovchain.go
@@ -1,9 +1,12 @@
 package markovChain
 
 import (
-	"fmt"
 	"errors"
+	"math/rand"
+	"time"
 )
+
+var randSource *rand.Rand
 
 type MarkovItem struct {
 	id string
@@ -12,21 +15,42 @@ type MarkovItem struct {
 
 type MarkovChain struct {
 	chain []MarkovItem
+	state *MarkovItem
 }
 
 func Create (ids []string, items [][]float64) (chain *MarkovChain, err error){
 	if len(ids) != len(items){
 		return nil, errors.New("Number of ids does not match the number of probability matrices.")
 	}
-	mc := MarkovChain{make([]MarkovItem, len(items), len(items))}
+
+	mc := MarkovChain{make([]MarkovItem, len(items), len(items)), nil}
 	for x,y := range items{
 		mi := MarkovItem{ids[x], y}
 		mc.chain[x] = mi
 	}
-	fmt.Println(mc)
+	mc.state = &mc.chain[0]
+
 	return &mc, nil
 }
 
 func (mc *MarkovChain) Next() (s string){
-	return ""
+	item := mc.state
+	x := item.Next()
+	mc.state = &mc.chain[x]
+	return mc.state.id
+}
+
+func (mi *MarkovItem) Next() (int){
+	if randSource == nil{
+		randSource = rand.New(rand.NewSource(time.Now().UnixNano()))
+	}
+	rVal := randSource.Float64()
+	p := 0.0
+	for i,x := range mi.connections{
+		if rVal < (float64(x) + p){
+			return i
+		}
+		p += x
+	}
+	return len(mi.connections) - 1
 }
--- markovchain_test.go
+++ markovchain_test.go
@@ -2,6 +2,7 @@ package markovChain
 
 import (
 	"testing"
+	"fmt"
 )
 
 func TestCreate(t *testing.T){
@@ -21,5 +22,25 @@ func TestCreate(t *testing.T){
 				{0.2, 0.2, 0.2, 0.2, 0.2}})
 	if mc == nil || err != nil{
 		t.Errorf("MarkovChain is nil")
+	}
+}
+
+func TestNext(t *testing.T){
+	mc, err := Create(
+			[]string{"a","b","c","d","e"},
+			[][]float64{
+				{0.2, 0.2, 0.2, 0.2, 0.2},
+				{0.2, 0.2, 0.2, 0.2, 0.2},
+				{0.2, 0.2, 0.2, 0.2, 0.2},
+				{0.2, 0.2, 0.2, 0.2, 0.2},
+				{0.2, 0.2, 0.2, 0.2, 0.2}})
+	if mc == nil || err != nil{
+		t.Errorf("Error created MarkovChain: MarkovChain is nil")
+	}
+
+	fmt.Println(mc.state.id)
+	for i := 0; i < 10; i++{
+		s := mc.Next()
+		fmt.Println(s)
 	}
 }
